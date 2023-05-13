90f1a3d8cbb555199239fba853709a3534ab4e3e
elnur-elnur <tahirzadee@gmail.com>
2023-05-13 14:04:11 +0400

    done action filter by brand

--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -1,8 +1,39 @@
 import { IProduct } from "@/interfaces/product";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const ProductCard = ({ product }: { product: IProduct }) => {
-  return <div>{product.price}</div>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="200"
+        image={product.thumbnail}
+        alt={product.title}
+      />
+      <CardContent>
+        <Typography variant="h5" color="text.secondary">
+          {product.title}
+        </Typography>
+        <Typography variant="body2">{product.description}</Typography>
+      </CardContent>
+      <CardActions
+        disableSpacing
+        sx={{ flexDirection: "column", alignItems: "flex-start" }}
+      >
+        <Button>Price: {product.price}</Button>
+        <Button>Rating: {product.rating}</Button>
+        <Button>Brand: {product.brand}</Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default ProductCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,70 @@
 import ProductsList from "@/components/ProductsList";
 import { IProduct } from "@/interfaces/product";
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
 import { Inter } from "next/font/google";
+import { useMemo, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ data }: { data: {products: IProduct[]} }) {
+export default function Home({ data }: { data: { products: IProduct[] } }) {
+  const [filteredProducts, setfilteredProducts] = useState([]);
+  const [brand, setBrand] = useState("");
+
+  const memoziedBrands = useMemo<string[]>(() => {
+    return [
+      ...new Set(data.products.map((product: IProduct) => product.brand)),
+    ];
+  }, []);
+
+  const handleChangeBrand = (e: SelectChangeEvent) => {
+    setBrand(e.target.value);
+
+    setfilteredProducts(
+      data.products.filter((product) => product.brand === e.target.value)
+    );
+  };
+
   return (
-    <Container sx={{ py: 2 }}>
+    <Container sx={{ py: 4 }}>
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <Typography>left</Typography>
+          <FormControl fullWidth>
+            <InputLabel>Brand</InputLabel>
+            <Select
+              id="demo-simple-select"
+              value={brand}
+              label="Age"
+              onChange={handleChangeBrand}
+            >
+              {memoziedBrands.map((brand: any) => {
+                return (
+                  <MenuItem key={brand} value={brand}>
+                    {brand}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
         </Grid>
         <Grid item xs={8}>
-          <Typography>right</Typography>
+          <Typography variant="h4" sx={{ mb: 3 }}>
+            Products
+          </Typography>
 
-          <ProductsList products={data.products} />
+          <ProductsList
+            products={
+              filteredProducts.length ? filteredProducts : data.products
+            }
+          />
         </Grid>
       </Grid>
     </Container>
